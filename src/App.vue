76fc0a24589a3f372d742020bccb4aeda6e58fee
Darius <42759541+LingXin0@users.noreply.github.com>
2023-06-08 10:49:33 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+        <WebOffice type="1" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import WebOffice from "./components/WebOffice.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        // HelloWorld,
+        WebOffice,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/WebOffice.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>{{ type }}</div>
+    ∑∑∑∑
+</template>
+
+<script>
+import webOfficeSdk from "../js/web-office-sdk-solution-v2.0.2.umd";
+
+export default {
+    name: "WebOffice",
+    props: {
+        type: String,
+    },
+    created() {
+        console.log(123);
+        const jssdk = webOfficeSdk.init({
+            // 必填项
+            officeType: webOfficeSdk.OfficeType.Writer,
+            appId: "SX20230607SAIWEY",
+            fileId: "335d5e0e520c4ec68fc2c65ad269146c",
+        });
+
+        // 需要等待 jssdk ready 之后再调用 API
+        jssdk.ready();
+    },
+};
+</script>
