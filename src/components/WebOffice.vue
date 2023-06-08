e0f281151805dbbf2aa7f74594e5cfccca9297b6
wwd01511522 <wwd01511522@alibaba-inc.com>
2023-06-08 11:32:44 +0800

    feat: 格式化

--- src/components/WebOffice.vue
+++ src/components/WebOffice.vue
@@ -1,27 +1,24 @@
 <template>
-    <div>{{ type }}</div>
-    ∑∑∑∑
+  <div>{{ type }}</div>
 </template>
 
 <script>
-import webOfficeSdk from "../js/web-office-sdk-solution-v2.0.2.umd";
+import WebOfficeSDK from '../js/web-office-sdk-solution-v2.0.2.umd'
 
 export default {
-    name: "WebOffice",
-    props: {
-        type: String,
-    },
-    created() {
-        console.log(123);
-        const jssdk = webOfficeSdk.init({
-            // 必填项
-            officeType: webOfficeSdk.OfficeType.Writer,
-            appId: "SX20230607SAIWEY",
-            fileId: "335d5e0e520c4ec68fc2c65ad269146c",
-        });
+  name: "WebOffice",
+  props: {
+    type: String,
+  },
+  mounted() {
+    const jssdk = WebOfficeSDK.init({
+      officeType: 'w',
+      appId: "SX20230607SAIWEY",
+      fileId: "335d5e0e520c4ec68fc2c65ad269146c",
+    });
+    console.log(jssdk)
 
-        // 需要等待 jssdk ready 之后再调用 API
-        jssdk.ready();
-    },
+    // 需要等待 jssdk ready 之后再调用 API
+  },
 };
 </script>
